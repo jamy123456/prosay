17936bdea0e829a33235c4ac3626212109012c4a
jamy123456 <990134943@qq.com>
2019-05-07 21:28:07 +0800

    1234

--- src/myprosay/MapListDemo.java
+++ src/myprosay/MapListDemo.java
@@ -14,6 +14,7 @@ import java.util.Map;
  */
 public class MapListDemo {
 	public static void main(String[] args) {
+		//
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("jamy", "12");
 		map.put("andy", "22");
